a8e944c7385f23b04022daca15a56937294c393b
Manish Ojha <mkomanishojha28@outlook.com>
2021-09-02 10:19:21 +0530

    converted class components into functional componets, used react hooks

--- src/Main.js
+++ src/Main.js
@@ -1,90 +1,90 @@
-import React, {Component} from 'react'
+import React, { useState, useEffect } from 'react'
 import { IconButton } from '@material-ui/core';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import './Main.css'
 import MapView from './MapView';
 
-class Main extends Component {
-    constructor(props) {
-        super()
-        this.state = {
-            latLong: {lat: 37.40599, lng: -122.078514},
-            ipAddress: ""
-        }
-    }
+const Main = () => {
 
-    componentDidMount() {
-        const ip = this.state.ipAddress
-        const url = `https://geo.ipify.org/api/v1?apiKey=at_2vTG07BaiN6spBlwtk2x6Y374lJd2&ipAddress=${ip}`
-        this.fetchApi(url, ip)
-    }
+    const [latLong, setLatLong] = useState({lat: 37.40599, lng: -122.078514})
+    const [ipAddress, setIpAddress] = useState("")
+    const [location, setLocation] = useState("")
+    const [isp, setIsp] = useState("")
+    const [timezone, setTimezone] = useState("")
+    const [ip, setIp] = useState("")
 
-    handleClick = () => {
-        let ip = document.querySelector("#ip_input").value
-        let url = `https://geo.ipify.org/api/v1?apiKey=at_2vTG07BaiN6spBlwtk2x6Y374lJd2&ipAddress=${ip}`
-        this.fetchApi(url, ip)
-        this.forceUpdate()
-    }
 
-    fetchApi(url, ip) {
+    useEffect(() => {
+        const url = `https://geo.ipify.org/api/v1?apiKey=at_2vTG07BaiN6spBlwtk2x6Y374lJd2&ipAddress=${ipAddress}`
         fetch(url)
+            .then(res => {
+                if(res.ok) {
+                    return res
+                }
+                else throw new Error("Not Found")
+            })
             .then((res) => res.json())
             .then((data) => {
                 const l1 = data.location.lat
                 const l2 = data.location.lng
                 const obj = {lat: l1, lng: l2}
-                if (l1 !== this.state.latLong.lat && l2 !== this.state.latLong.lng) {
-                    this.setState({
-                        latLong: obj,
-                        ipAddress: data.ip,
-                        location: data.location.region,
-                        isp: data.isp,
-                        timezone: `UTC ${data.location.timezone}`
-                    })
+                if(l1 !== undefined && l2 !== undefined){
+                    setLatLong(obj)
                 }
-                console.log(data);
+                setIpAddress(data.ip)
+                setLocation(data.location.region)
+                setIsp(data.isp)
+                setTimezone(`UTC ${data.location.timezone}`)
             })
+            .catch()
+    }, [ipAddress])
+
+    const handleClick = () => {
+        setIpAddress(ip)
     }
 
+    const handleChange = (e) => {
+        setIp(e.target.value)
+    }
 
-    render() {
-        return (
-            <div className="main_container">
-                <div className="upper_box">
-                    <h2 id="top">IP Address Tracker</h2>
-                    <div className="search_container">
-                        <input
-                            id="ip_input" 
-                            type="text" 
-                            placeholder="Search for any IP address or domain"
-                            on></input>
-                        <IconButton id="btn" onClick={this.handleClick}>
-                            <ArrowForwardIosIcon />
-                        </IconButton>
-                    </div> 
-                </div>
-                <div className="data_card">
-                    <div className="data">
-                        <p className="small">IP ADDRESS</p>
-                        <p className="large">{this.state.ipAddress}</p>
-                    </div>
-                    <div className="data">
-                        <p className="small">LOCATION</p>
-                        <p className="large">{this.state.location}</p>
-                    </div>
-                    <div className="data">
-                        <p className="small">TIMEZONE</p>
-                        <p className="large">{this.state.timezone}</p>
-                    </div>
-                    <div className="data">
-                        <p className="small">ISP</p>
-                        <p className="large">{this.state.isp}</p>
-                    </div>
-                </div>
-                <MapView {...this.state}/>
+    return (
+        <div className="main_container">
+            <div className="upper_box">
+                <h2 id="top">IP Address Tracker</h2>
+                <div className="search_container">
+                    <input
+                        id="ip_input" 
+                        type="text" 
+                        placeholder="Search for any IP address or domain"
+                        onChange={e => handleChange(e)}
+                        ></input>
+                    <IconButton id="btn" onClick={handleClick}>
+                        <ArrowForwardIosIcon />
+                    </IconButton>
+                </div> 
             </div>
-        )
-    }
+            <div className="data_card">
+                <div className="data">
+                    <p className="small">IP ADDRESS</p>
+                    <p className="large">{ipAddress}</p>
+                </div>
+                <div className="data">
+                    <p className="small">LOCATION</p>
+                    <p className="large">{location}</p>
+                </div>
+                <div className="data">
+                    <p className="small">TIMEZONE</p>
+                    <p className="large">{timezone}</p>
+                </div>
+                <div className="data">
+                    <p className="small">ISP</p>
+                    <p className="large">{isp}</p>
+                </div>
+            </div>
+            <MapView latLong={latLong}/>
+        </div>
+    )
+
 }
 
 export default Main
--- src/MapView.js
+++ src/MapView.js
@@ -1,62 +1,46 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Leaflet from 'leaflet'
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import icon from './images/icon-location.svg';
 
-const DefaultIcon = Leaflet.icon({
-    iconUrl: icon,
-    iconSize: [27, 40],
-    popupAnchor: [0, -46]
-});
+const MapView = ({ latLong }) => {
 
-const CenterView = ({ center }) => {
-    const map = useMap()
-    map.setView(center)
-    return null
+    const DefaultIcon = Leaflet.icon({
+        iconUrl: icon,
+        iconSize: [27, 40],
+        popupAnchor: [0, -46]
+    });
+
+    const CenterView = ({ center }) => {
+        const map = useMap()
+        map.setView(center)
+        return null
+    }
+
+    const position = Object.values(latLong)
+
+    return (
+        <MapContainer 
+            center={position}
+            zoom={11}
+            style={{ height: "70vh", width: "100%", zIndex: "1", pointerEvents: "auto" }}
+            scrollWheelZoom={true}
+            zoomControl={false}
+            zoomAnimation={true}
+            touchZoom={"center"}
+            >
+                <CenterView center={position} />
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position} icon={DefaultIcon}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+        </MapContainer>
+    )
 }
 
-
-export default class MapView extends Component{
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            latLong: this.props.latLong,
-        }   
-    }
-
-    static getDerivedStateFromProps(props, state) {
-        return {latLong: props.latLong}
-    }
-
-    
-    render() {
-        const position = Object.values(this.state.latLong)
-        return (
-            <MapContainer 
-                center={position}
-                zoom={11}
-                style={{ height: "70vh", width: "100%", zIndex: "1", pointerEvents: "auto" }}
-                scrollWheelZoom={true}
-                zoomControl={false}
-                zoomAnimation={true}
-                touchZoom={"center"}
-                >
-                    <CenterView center={position} />
-                    <TileLayer
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <Marker position={position} icon={DefaultIcon}>
-                        <Popup>
-                            A pretty CSS3 popup. <br /> Easily customizable.
-                        </Popup>
-                    </Marker>
-                    {console.log(this.state.latLong)}
-            </MapContainer>
-        )
-    }
-}
-
-
-
+export default MapView
